8ab375216ba3633e0f1ed0407514d6b60203587b
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-17 19:50:07 +0200

    Logic game and inner html

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // - viasualizzare in pagina un alert con 5 numeri
 //     - dichiarare una variabile che contenga un array vuoto
+const scoreDOMElement = document.querySelector(".score");
+console.log(scoreDOMElement)
 const numbersArray = [];
 
 const arrayGenerator = getArrayRandom(1, 50, 5);
@@ -25,15 +27,42 @@ function getArrayRandom (minRange, maxRange, number) {
     }
     console.log(numbersArray);
 }
-    
+
+// - dichiaro un array vuoto con i numeri che inserisce l'utente
+const userArray = [];
+let score = 0;    
 // - far partire un timer di 30 sec 
 //     - invocare una timing function di 30 secondi
 //     - all'interno della timing function invochiamo una funzione che genererà 5 comandi prompt
+setTimeout(logicGame, 3000);
 
-// - dopodiche l'utente dovrà inserire tramite prompt i numeri visti uno per volta
-//     - dichiarare una funzione che genererà 5 promt
-//     - all'interno della funzione generiamo 5 prompt in un ciclo for
 //     - il risultato di questi 5 prompt verrà salvato un un array vuoto
+
 // - alla fine il software dirà quanti e quali numeri sono stati individuati
 //     - confronteremo il contenuto dei due array che avremo ottenuto dal pc e dall'utente
 //     - stampiamo in un alert i risultati
+ 
+
+// - dopodiche l'utente dovrà inserire tramite prompt i numeri visti uno per volta
+//     - dichiarare una funzione che genererà 5 promt
+function logicGame() {
+    //     - all'interno della funzione generiamo 5 prompt in un ciclo for
+    for (let i = 0; i < 5; i++) {
+        const userNumberRequest = prompt('quale numero ricordi?');
+        const userNumber = parseInt(userNumberRequest);
+
+        if (numbersArray.includes(userNumber) && !userArray.includes(userNumber)) {
+            userArray.push(userNumber)
+            score++
+            const scoreContent = `hai indovinato ${score} numeri: ${userArray}`
+            console.log(score, scoreContent);
+            scoreDOMElement.innerHTML = scoreContent;
+        } else if (score === 0) {
+            scoreDOMElement.innerHTML = "non sai fare la o col bicchiere"
+        }
+    }
+}
+
+
+
+

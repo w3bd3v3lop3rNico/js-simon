36a01a4017f03a1c7f2129cf8234b2855388681d
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-18 10:35:34 +0200

    fix comments and time functions timing

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@
 //     - dichiarare una variabile che contenga un array vuoto
 const scoreDOMElement = document.querySelector(".score");
 console.log(scoreDOMElement)
+
 const numbersArray = [];
 
-const arrayGenerator = getArrayRandom(1, 50, 5);
+getArrayRandom(1, 50, 5);
 
-console.log(numbersArray);
 alert(numbersArray);
 
 //     - dichiarare una funzione che generi numeri random
@@ -30,30 +30,33 @@ function getArrayRandom (minRange, maxRange, number) {
 
 // - dichiaro un array vuoto con i numeri che inserisce l'utente
 const userArray = [];
-let score = 0;    
+
+// - dichiaro una variabile score globale da incrementare dentro al controllo
+let score = 0;
+
 // - far partire un timer di 30 sec 
 //     - invocare una timing function di 30 secondi
-//     - all'interno della timing function invochiamo una funzione che genererà 5 comandi prompt
-setTimeout(logicGame, 3000);
-
-//     - il risultato di questi 5 prompt verrà salvato un un array vuoto
-
-// - alla fine il software dirà quanti e quali numeri sono stati individuati
-//     - confronteremo il contenuto dei due array che avremo ottenuto dal pc e dall'utente
-//     - stampiamo in un alert i risultati
- 
+//     - all'interno della timing function invochiamo una funzione della logica di gioco
+setTimeout(logicGame, 30000);
 
 // - dopodiche l'utente dovrà inserire tramite prompt i numeri visti uno per volta
-//     - dichiarare una funzione che genererà 5 promt
+//     - dichiarare una funzione che contiene la logica
 function logicGame() {
     //     - all'interno della funzione generiamo 5 prompt in un ciclo for
     for (let i = 0; i < 5; i++) {
         const userNumberRequest = prompt('quale numero ricordi?');
         const userNumber = parseInt(userNumberRequest);
-
+        // - alla fine il software dirà quanti e quali numeri sono stati individuati
+        //     - confronteremo il contenuto dei due array che avremo ottenuto dal pc e dall'utente
         if (numbersArray.includes(userNumber) && !userArray.includes(userNumber)) {
-            userArray.push(userNumber)
+
+            //     - il risultato di questi 5 prompt verrà salvato un un array vuoto
+            userArray.push(userNumber);
+
+            //     - incremento lo score
             score++
+
+            //     - stampinamo i risultati
             const scoreContent = `hai indovinato ${score} numeri: ${userArray}`
             console.log(score, scoreContent);
             scoreDOMElement.innerHTML = scoreContent;
